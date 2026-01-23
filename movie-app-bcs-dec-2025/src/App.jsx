01ee897f3c93f9aa7fee526a312660f318951167
Dhanapalmani20 <dhanapalmani15@gmail.com>
2026-01-23 17:18:01 +0530

    users task

--- movie-app-bcs-dec-2025/src/App.jsx
+++ movie-app-bcs-dec-2025/src/App.jsx
@@ -4,23 +4,29 @@ import { User } from "./User";
 //Component = UI + Logic
 export default function App() {
   //Logic Start
+  // return (
+  //UI Starts
+  // <div className="App">
+  const users = [
+    {
+      name: "Arjun",
+      url: "https://image.gala.de/21270186/t/0U/v5/w960/r0.6667/-/elon-musk--11584341-.jpg",
+    },
+
+    {
+      name: "mani",
+      url: "https://image.gala.de/21270186/t/0U/v5/w960/r0.6667/-/elon-musk--11584341-.jpg",
+    },
+    {
+      name: "Saravana",
+      url: "https://image.gala.de/21270186/t/0U/v5/w960/r0.6667/-/elon-musk--11584341-.jpg",
+    },
+  ];
   return (
-    //UI Starts
     <div className="App">
-      <User
-        name="Arjun"
-        img="https://image.gala.de/21270186/t/0U/v5/w960/r0.6667/-/elon-musk--11584341-.jpg"
-      />
-      <User
-        name="mani"
-        img="https://image.gala.de/21270186/t/0U/v5/w960/r0.6667/-/elon-musk--11584341-.jpg"
-      />
-      <User
-        name="Saravana"
-        img="https://image.gala.de/21270186/t/0U/v5/w960/r0.6667/-/elon-musk--11584341-.jpg"
-      />
+      {users.map((user) => (
+        <User name={user.name} url={user.url}/>
+      ))}
     </div>
   );
-
- 
 }
