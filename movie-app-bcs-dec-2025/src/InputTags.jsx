a18283bb9b8b324bed125f81c1cec08dca8f3b2f
Dhanapalmani20 <dhanapalmani15@gmail.com>
2026-02-01 19:33:20 +0530

    movie task

---
+++ movie-app-bcs-dec-2025/src/InputTags.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function InputTags() {
+  const [like, setLike] = useState(0);
+  const [Dislike, setDisLike] = useState(0);
+
+  return (
+    <div className="like-btn">
+      <button className="like" onClick={() => setLike(like + 1)}>
+        {like}👍
+      </button>
+      <button className="dislike" onClick={() => setDisLike(Dislike + 1)}>
+        {Dislike}👎
+      </button>
+      <div>
+        {/* {{like >= disLike + 10 && <h2>You are awesome 🎊🎊</h2>}} */}
+        {/* <progress max={like+Dislike} value={like}></progress>  */}
+      </div>
+    </div>
+  );
+}
+
+export default InputTags;
